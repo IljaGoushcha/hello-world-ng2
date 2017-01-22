7ff8685aa34057d20d9faf8d66a144e27e562572
Ilja Goushcha <iljagou@yahoo.com>
2017-01-21 23:16:06 -0800

    created my first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {ContactInfoComponent} from "./contact-info/contact-info.component";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactInfoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/contact-info/contact-info.component.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by ig on 1/21/17.
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'contact-info',
+  templateUrl: './contact-info.component.html'
+  // styleUrls: ['./app.component.css']
+})
+export class ContactInfoComponent {
+  message: string = 'app works!';
+}
